dcfe4f7cc1705a9af99914bd556747627f52163b
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-04-29 14:07:08 +0100

    style: Change header color when scroll
    # Conflicts:
    #	src/pages/Accounts/Modals/SendFunds.tsx

--- src/pages/Accounts/Modals/SendFunds.tsx
+++ src/pages/Accounts/Modals/SendFunds.tsx
@@ -4,8 +4,7 @@ import styled from 'styled-components/macro';
 
 import { TTransferFunds } from './types';
 import { useAccounts } from '../../../hooks/useAccounts';
-import DefaultAvatar from '../../../static/icons/default-avatar.svg';
-import { AdditionalWarning100 } from '../../../styles/colors';
+import { AdditionalWarning100, Primary700 } from '../../../styles/colors';
 import { SelectInput } from '../../../components/SelectInput/SelectInput';
 import { Account } from '../../../account/AccountContext';
 import DefaultMarketStages from '../../Token/Modals/StagesModal';
@@ -190,7 +189,7 @@ const TextStyled = styled(Text)`
   padding: 8px 16px;
   margin: calc(var(--gap) * 1.5) 0;
   border-radius: 4px;
-  background-color: ${AdditionalWarning100};
+  background-color: ${Primary700};
   width: 100%;
 `;
 
