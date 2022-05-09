0d37261652da4bfcbb0da6875ea8341ae57bb098
GiovanneGuerra <giovanneguerra@hotmail.com>
2022-05-09 11:30:24 +0100

    chore: Change environment file

--- public/env.js
+++ public/env.js
@@ -4,6 +4,6 @@
   w.ENV.NET_QUARTZ_NAME = String('QUARTZ by UNIQUE'); 
   w.ENV.SCAN_ACCOUNT_URL = String('https://uniquescan.io/QUARTZ/account/'); 
   w.ENV.SCAN_URL = String('https://uniquescan.io/QUARTZ/'); 
-  w.ENV.UNIQUE_API_URL = String('https://api.sadu.sovereignnature.ch/');
+  w.ENV.UNIQUE_API_URL = String('https://api.compendium.sovereignnature.ch');
   w.ENV.VERSION = String('master');
 })(this);
